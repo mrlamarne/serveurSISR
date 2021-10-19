8660f3d8c13d19fde7505bd4107568cf610c2af1
Remi La Marne <remi.la-marne@ac-amiens.fr>
2021-10-19 11:49:53 +0200

    Premier commit initiale

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>Ressources SISR 2021-2022</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid">
+<h1>Serveur SISR 2021-2022</h1>
+<h2>Ressources présentes sur le serveur:</h2>
+  <a href="https://10.1.42.1/ressources"><b>Accés aux ressources</b></a>
+</div>
+
+<div class="container-fluid">
+    <h2>Guide d'utilisation : </h2>
+    <p>
+        Ce serveur regroupes différentes ressources pour les SISR, il est maintenu à jour par la promo SIO2-SISR 2021-2022( les admin sont Hanafi, David, Mathis et Nathan<br />
+        Pour ajouter des ressources sur ce serveur il faudra dropper via FTP.
+        <br /><a href="http://10.1.42.1:9090">Un service Cockpit est disponible sur le port <b>9090</b></a>
+    </p>
+        
+</div>
+
+</body>
+</html> 
